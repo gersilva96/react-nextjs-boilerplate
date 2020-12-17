2b6de2e39db917e6aefb2ac642c0a4f3c36a5b2e
Germán Silva <gersilva96@hotmail.com>
2020-12-17 10:26:11 -0300

    Add Link components

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -4,21 +4,30 @@
  * It shows the views according to the url.
  */
 
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-import ROUTES from '~/constants/routes';
+import React, { FunctionComponent } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ROUTES from '@constants/routes';
+import CounterScreen from '@views/screens/CounterScreen';
 import styles from './index.scss';
 
-const Router: React.FunctionComponent = () => {
+const Router: FunctionComponent = () => {
   return (
     <div className={styles.mainContainer}>
       <BrowserRouter>
         <Route exact path={ROUTES.MAIN}>
           <div>CACA</div>
+          <Link to={ROUTES.COUNTER}>
+            <button>Counter Screen</button>
+          </Link>
+          <Link to={'/test2'}>
+            <button>Test2 route</button>
+          </Link>
         </Route>
         <Route exact path={'/test2'}>
           <div>CACA2</div>
+        </Route>
+        <Route exact path={ROUTES.COUNTER}>
+          <CounterScreen />
         </Route>
       </BrowserRouter>
     </div>
