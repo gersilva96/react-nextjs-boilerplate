bf752f8495b076c9e89c1dfc0bc1383f8e134633
Germán Silva <gersilva96@hotmail.com>
2021-01-04 21:37:39 -0300

    Added NasaApod screen

---
+++ src/views/screens/NasaApod.tsx
@@ -0,0 +1,14 @@
+/**
+ * @packageDocumentation
+ * @module Views/Screens/NasaApod
+ * Contains the NasaApod screen.
+ */
+
+import React from 'react';
+import NasaApod from '~/views/components/NasaApod';
+
+const NasaApodScreen = (): JSX.Element => {
+  return <NasaApod />;
+};
+
+export default NasaApodScreen;
