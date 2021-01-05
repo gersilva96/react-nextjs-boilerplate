df4e75ea50f555e2fd898f65009b9ae5db31812d
Germán Silva <gersilva96@hotmail.com>
2021-01-04 21:26:09 -0300

    Updated controllers

---
+++ src/controller/nasaApod.ts
@@ -0,0 +1,52 @@
+/**
+ * @packageDocumentation
+ * @module Controller/NasaApod
+ * Interacts with the NasaApod state.
+ */
+
+import nasaApodActions from '~/state/actions/nasaApod';
+import nasaApodService from '~/services/nasaApod';
+
+const nasaApodController = {
+  /**
+   * Gets the image link.
+   */
+  getImage: (): string => nasaApodActions.getImage(),
+
+  /**
+   * Gets the date value.
+   */
+  getDate: (): string => nasaApodActions.getDate(),
+
+  /**
+   * Gets the status of loading
+   */
+  getLoading: (): boolean => nasaApodActions.getLoading(),
+
+  /**
+   * Sets and gets a new image.
+   */
+  setImage: async (): Promise<string> => {
+    try {
+      const date = nasaApodController.getDate();
+      nasaApodActions.setLoading(true);
+      const img: string = await nasaApodService.get(date);
+      nasaApodActions.setImage(img);
+      nasaApodActions.setLoading(false);
+    } catch (error) {
+      nasaApodActions.setImage('');
+    }
+    return nasaApodController.getImage();
+  },
+
+  /**
+   * Sets and gets the date value.
+   * @param date Date to be setted.
+   */
+  setDate: (date: string): string => {
+    nasaApodActions.setDate(date);
+    return nasaApodActions.getDate();
+  },
+};
+
+export default nasaApodController;
