90d71b86526efbc411b99c95afe445f9354d0fea
Germán Silva <gersilva96@hotmail.com>
2021-02-04 16:27:20 -0300

    Updated all application style

--- src/views/components/NasaApod/DateInput/index.tsx
+++ src/views/components/NasaApod/DateInput/index.tsx
@@ -6,27 +6,42 @@
 
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import DatePicker from 'react-datepicker';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import moment from 'moment';
 import { nasaApodSelector } from '~/state/features/nasaApodSlice';
 import nasaApodController from '~/controller/nasaApod';
 import styles from '../index.scss';
+import i18n from '~/internationalization';
 
 const DateInput = (): JSX.Element => {
   const { date } = useSelector(nasaApodSelector);
-  const [startDate, setStartDate] = useState(new Date(moment(date).format()));
-  const handleChange = (newDate: Date) => {
+  const [startDate, setStartDate] = useState<Date>(new Date(moment(date).format()));
+  const handleDateChange = (changedDate: MaterialUiPickersDate) => {
+    const formattedDate = changedDate
+      ? changedDate.format('YYYY-MM-DD')
+      : moment().format('YYYY-MM-DD');
+    const newDate = new Date(moment(formattedDate).format());
     setStartDate(newDate);
-    nasaApodController.setDate(moment(newDate).format('YYYY-MM-DD'));
+    nasaApodController.setDate(formattedDate);
   };
+
   return (
-    <DatePicker
-      dateFormat="yyyy-MM-dd"
-      selected={startDate}
-      todayButton="Today"
-      onChange={handleChange}
-      className={styles.dateInput}
-    />
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <KeyboardDatePicker
+        margin="normal"
+        id="date-picker-dialog"
+        label={i18n.get('NASAAPOD_SELECT_DATE')}
+        format="DD/MM/yyyy"
+        value={startDate}
+        onChange={handleDateChange}
+        KeyboardButtonProps={{
+          'aria-label': 'change date',
+        }}
+        className={styles.dateInput}
+      />
+    </MuiPickersUtilsProvider>
   );
 };
 
--- src/views/components/NavBar/index.tsx
+++ src/views/components/NavBar/index.tsx
@@ -4,30 +4,95 @@
  * UI that has the NavBar.
  */
 
-import React from 'react';
+import React, { FunctionComponent, useState, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  SwipeableDrawer,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import CONSTANTS from '~/constants';
 import styles from './index.scss';
 
 const { ROUTES } = CONSTANTS;
 
-const NavBar = (): JSX.Element => {
+const NavBar: FunctionComponent = () => {
+  const [opened, setOpened] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpened(!opened);
+  };
+
   return (
-    <div className={styles.navbar}>
-      <Link to={ROUTES.MAIN}>
-        <div className={styles.headerOption}>Main</div>
-      </Link>
-      <Link to={ROUTES.COUNTER}>
-        <div className={styles.headerOption}>Counter</div>
-      </Link>
-      <Link to={ROUTES.TODOS}>
-        <div className={styles.headerOption}>TODOS</div>
-      </Link>
-      <Link to={ROUTES.NASA_APOD}>
-        <div className={styles.headerOption}>NASA API</div>
-      </Link>
-    </div>
+    <Fragment>
+      <SwipeableDrawer anchor={'left'} open={opened} onClose={toggleDrawer} onOpen={toggleDrawer}>
+        <div className={styles.drawer} onClick={toggleDrawer} onKeyDown={toggleDrawer}>
+          <h1 className={styles.title}>Template Front</h1>
+          <div className={styles.drawerLinks}>
+            <Link className={styles.navButton} to={ROUTES.MAIN}>
+              <Button color="primary" size="large" variant="contained">
+                Home
+              </Button>
+            </Link>
+            <Link className={styles.navButton} to={ROUTES.COUNTER}>
+              <Button color="primary" size="large" variant="contained">
+                Counter
+              </Button>
+            </Link>
+            <Link className={styles.navButton} to={ROUTES.TODOS}>
+              <Button color="primary" size="large" variant="contained">
+                Todos
+              </Button>
+            </Link>
+            <Link className={styles.navButton} to={ROUTES.NASA_APOD}>
+              <Button color="primary" size="large" variant="contained">
+                Nasa Apod
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </SwipeableDrawer>
+      <AppBar position="fixed" className={styles.appbar}>
+        <Toolbar className={styles.toolbar}>
+          <IconButton
+            edge="start"
+            className={styles.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Template Front</Typography>
+          <div className={styles.navLinks}>
+            <Link className={styles.navButton} to={ROUTES.MAIN}>
+              <Button color="inherit" size="large" variant="text">
+                Home
+              </Button>
+            </Link>
+            <Link className={styles.navButton} to={ROUTES.COUNTER}>
+              <Button color="inherit" size="large" variant="text">
+                Counter
+              </Button>
+            </Link>
+            <Link className={styles.navButton} to={ROUTES.TODOS}>
+              <Button color="inherit" size="large" variant="text">
+                Todos
+              </Button>
+            </Link>
+            <Link className={styles.navButton} to={ROUTES.NASA_APOD}>
+              <Button color="inherit" size="large" variant="text">
+                Nasa Apod
+              </Button>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
   );
 };
 
