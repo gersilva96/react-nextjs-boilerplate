428f70f5b28a73bae8617aceba823ff7ee4d31e6
Germán Silva <gersilva96@hotmail.com>
2021-02-09 20:16:46 -0300

    Added favicon

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -65,6 +65,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
+      favicon: path.resolve(__dirname, 'public', 'images', 'favicon.png'),
     }),
   ],
   devServer: {
